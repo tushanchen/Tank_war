74abeaa5f5b4301c92f8b0289b47854749e2e8fb
tushanchen <tushanchen@bytedance.com>
2019-12-15 23:22:52 +0800

    change common wall in pc

--- src/CommonWall.java
+++ src/CommonWall.java
@@ -1,10 +1,10 @@
 import java.awt.*;
-
+//change in pc
 public class CommonWall {
 	public static final int width = 20; //ÉèÖÃÇ½µÄ¹Ì¶¨²ÎÊý
 	public static final int length = 20;
 	int x, y;
-
+//hhhhhpcpcpcpcp
 	TankClient tc;
 	private static Toolkit tk = Toolkit.getDefaultToolkit();
 	private static Image[] wallImags = null;
@@ -12,18 +12,18 @@ public class CommonWall {
 		wallImags = new Image[] { // ´¢´æcommonWallµÄÍ¼Æ¬
 		tk.getImage(CommonWall.class.getResource("Images/commonWall.gif")), };
 	}
-
 	public CommonWall(int x, int y, TankClient tc) { // ¹¹Ôìº¯Êý
 		this.x = x;
 		this.y = y;
-		this.tc = tc; // »ñµÃ½çÃæ¿ØÖÆ
+		this.tc = tc; // »ÖÆ
 	}
 
 	public void draw(Graphics g) {// »­commonWall
 		g.drawImage(wallImags[0], x, y, null);
 	}
 
-	public Rectangle getRect() {  //¹¹ÔìÖ¸¶¨²ÎÊýµÄ³¤·½ÐÎÊµÀý
+	public Rectangle getRect() {  //delete you pc
 		return new Rectangle(x, y, width, length);
 	}
+
 }
